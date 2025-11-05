b268e37cdbcba739bafb04c489c9a0ee37a0ac58
itu-itis25-Mohammed-Alashi <alashim24@itu.edu.tr>
2025-11-05 18:49:34 +0300

    Add reviews page and templates

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    print("hello blg 317 friends")
+    return "<h1>Welcome To Popcoders Project</h1>"
+
+@app.route("/reviews")
+def reviews():
+    return render_template("reviews.html")
 
 
+if __name__ == '__main__':
+    app.run(host="0.0.0.0" , port=8080 , debug=True)
