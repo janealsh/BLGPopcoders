2ad12a128ddad67e95b0cf38e53314fcf4adbab1
janealsh <janealshihabi@gmail.com>
2025-12-19 13:56:17 +0300

    add delete endpoint for watch_history

--- app/views.py
+++ app/views.py
@@ -16,5 +16,19 @@ def watch_history():
     # return render_template("watch_history.html", watch_history = watch_history)
     return render_template("watch_history.html")
 
+def delete_watch_history():
+
+    try: 
+        wh_id = request.form.get("primary_key")
+
+        query = """DELETE FROM watch_history WHERE session_id = %s"""
+        cursor.execute(query, (wh_id,))
+        db.commit()
+
+        return "Watch history entry deleted successfully. <a href='/watch_history'>Back to Watch History</a>"
+
+    except Exception as e:
+        return f"Error deleting entry: {e}"
+
 def recommend():
     return render_template("recommend.html")
